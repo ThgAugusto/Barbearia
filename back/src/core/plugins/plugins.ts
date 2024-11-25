79bed82b36ebe2384d062721aa034c2047a771ba
CaiqueOrtega <kayqueortega@gmail.com>
2024-11-25 18:39:10 -0300

    feat: implement authentication middleware with role-based checks
    - Created `authenticate` middleware to verify JWT and handle authentication errors, including session expiration, invalid token, and unauthorized access.
    - Added `checkUserRole` function to enforce role-based authorization for user access control.

--- back/src/core/plugins/plugins.ts
+++ back/src/core/plugins/plugins.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import fastifyCors from '@fastify/cors';
 import fastifyJwt from '@fastify/jwt';
 
+
 export function setupPlugins(server: FastifyInstance): void {
     server.register(fastifyCors, {
         origin: '*',
@@ -10,4 +11,5 @@ export function setupPlugins(server: FastifyInstance): void {
     server.register(fastifyJwt, {
         secret: 'f185e378b592b4fcce6d686ffafe092bb6d6011edd76be3a9e817fc3045b24d6', 
     });
+
 }

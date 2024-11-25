922cda0012decafcb3f7c5179881fad17086a2b4
CaiqueOrtega <kayqueortega@gmail.com>
2024-11-25 18:42:42 -0300

    refactor: refactor user routes to use auto-registration and update delete route to softDelete

--- back/src/routes/user.routes.ts
+++ back/src/routes/user.routes.ts
@@ -1,31 +1,33 @@
 import { FastifyInstance } from 'fastify';
-import { injectable, inject } from 'tsyringe';
 import { UserController } from '../modules/controllers/user.controller';
 import { CreateUserDTO, UpdateUserDTO } from '../modules/dtos/user.dto';
+import { AutoRegister, inject } from '../utils/auto-register.decorator';
 
-@injectable()
+@AutoRegister()
 export class UserRoutes {
-  constructor(@inject(UserController) private userController: UserController) { }
+  constructor(
+    @inject(UserController) private userController: UserController,
+    @inject('FastifyInstance') private readonly fastify: FastifyInstance) { }
 
-  public register(fastify: FastifyInstance) {
-    fastify.post<{ Body: CreateUserDTO }>('/users', async (request, reply) => {
+  public register() {
+    this.fastify.post<{ Body: CreateUserDTO }>('/users', async (request, reply) => {
       return this.userController.create(request, reply);
     });
 
-    fastify.get('/users', async (request, reply) => {
+    this.fastify.get('/users', async (request, reply) => {
       return this.userController.findAll(request, reply);
     });
 
-    fastify.get<{ Params: { id: number } }>('/users/:id', async (request, reply) => {
+    this.fastify.get<{ Params: { id: number } }>('/users/:id', async (request, reply) => {
       return this.userController.findById(request, reply);
     });
 
-    fastify.put<{ Params: { id: number }; Body: UpdateUserDTO }>('/users/:id', async (request, reply) => {
+    this.fastify.put<{ Params: { id: number }; Body: UpdateUserDTO }>('/users/:id', async (request, reply) => {
       return this.userController.update(request, reply);
     });
 
-    fastify.delete<{ Params: { id: number } }>('/users/:id', async (request, reply) => {
-      return this.userController.delete(request, reply);
+    this.fastify.delete<{ Params: { id: number } }>('/users/:id', async (request, reply) => {
+      return this.userController.softDelete (request, reply);
     });
   }
 }

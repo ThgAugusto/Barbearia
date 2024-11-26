0cbe1382fdf8617a22dd3f27387e7b0fbc973773
CaiqueOrtega <kayqueortega@gmail.com>
2024-11-26 01:42:09 -0300

    feat(plugins): setup CORS, cookie, and JWT plugins for server configuration
    - Registered `fastifyCors` plugin to allow cross-origin requests from 'http://localhost:5173'.
    - Configured `fastifyCookie` for cookie parsing and management with an empty secret.
    - Added `fastifyJwt` for JWT authentication, with a secret and configuration for the token cookie.

--- back/src/core/plugins/plugins.ts
+++ back/src/core/plugins/plugins.ts
@@ -1,15 +1,28 @@
 import { FastifyInstance } from 'fastify';
 import fastifyCors from '@fastify/cors';
 import fastifyJwt from '@fastify/jwt';
+import { fastifyCookie } from '@fastify/cookie';
 
 
 export function setupPlugins(server: FastifyInstance): void {
     server.register(fastifyCors, {
-        origin: '*',
+        origin: 'http://localhost:5173',
+        credentials: true,
     });
 
-    server.register(fastifyJwt, {
-        secret: 'f185e378b592b4fcce6d686ffafe092bb6d6011edd76be3a9e817fc3045b24d6', 
+    server.register(fastifyCookie, {
+        secret: '',
+        parseOptions: {
+        }
     });
+    
+    server.register(fastifyJwt, {
+        secret: 'Tz8JvQk2!7XxCm@r5PzVd1Wq+9Fb2KzUe@L1mYx8KoVdBz3LzA!V0eF2HtQ9E&',
+        cookie: {
+            cookieName: 'token',
+            signed: false,
+        }
+    });
+
 
 }

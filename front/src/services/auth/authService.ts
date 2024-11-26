dee658c7edc3aab0635e9540c050e2ed27b42ff6
CaiqueOrtega <kayqueortega@gmail.com>
2024-11-26 01:38:57 -0300

    feat(auth): implement AuthServices for user authentication and verification
    - Created `AuthServices` module to handle user authentication and session verification.
    - Added `authenticate` method to send login requests with email and password.
    - Added `verifyAuth` method to check if the user is authenticated via a server-side check.
    - Integrated `ConnectionAPI` for making authenticated HTTP requests.

---
+++ front/src/services/auth/authService.ts
@@ -0,0 +1,18 @@
+import ConnectionAPI from "../api/connectionAPI";
+
+function AuthServices() {
+
+    const authenticate = async (email: string, password: string) => {
+        const response = await ConnectionAPI.post('auth', {email, password,});
+        return response.data;
+    }
+
+    const verifyAuth = async () => {
+        const response = await ConnectionAPI.get('/auth/verify')
+        return response.data;
+    }
+
+    return { authenticate, verifyAuth };
+}
+
+export default AuthServices;

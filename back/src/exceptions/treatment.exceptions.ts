970b653f0b5d2ef1931d9952a5c3ebf4cd3ba4ee
CaiqueOrtega <kayqueortega@gmail.com>
2024-11-29 02:29:56 -0300

    refactor(treatment-errors): remove TreatmentValidationError class

--- back/src/exceptions/treatment.exceptions.ts
+++ back/src/exceptions/treatment.exceptions.ts
@@ -14,10 +14,3 @@ export class TreatmentNotFoundError extends CustomError {
         super(message, 404);
     }
 }
-
-export class TreatmentValidationError extends CustomError {
-    constructor(error: ZodError) {
-        const message = 'Erro de validação de tratamento';
-        super(message, 400, error.format());
-    }
-}

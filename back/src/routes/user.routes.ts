d418acef48576b363a0839285e0eabcf1b877b0b
CaiqueOrtega <caique.ortega.dev@gmail.com>
2024-11-29 02:47:08 -0300

    feat(userRoutes): add role-based user creation and restore functionality
    - Added `/users/owner` for creating owners and `/users/barber` for creating barbers with authentication.
    - Added `/user/barbers/barbershop/:barbershopId` to list barbers by barbershop ID.
    - Added `/users/:id/restore` route to restore a soft-deleted user.

--- back/src/routes/user.routes.ts
+++ back/src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { UserController } from '../modules/controllers/user.controller';
 import { CreateUserDTO, UpdateUserDTO } from '../modules/dtos/user.dto';
 import { AutoRegister, inject } from '../utils/auto-register.decorator';
+import { authenticate, checkUserRole } from '../core/middlewares/auth.middlewares';
 
 @AutoRegister()
 export class UserRoutes {
@@ -10,8 +11,23 @@ export class UserRoutes {
     @inject('FastifyInstance') private readonly fastify: FastifyInstance) { }
 
   public register() {
-    this.fastify.post<{ Body: CreateUserDTO }>('/users', async (request, reply) => {
-      return this.userController.create(request, reply);
+    const authMiddleware = [authenticate, checkUserRole('OWNER')];
+
+    this.fastify.post<{ Body: CreateUserDTO }>('/users/owner', async (request, reply) => {
+      return this.userController.createOwner(request, reply);
+    });
+
+    this.fastify.post<{ Body: CreateUserDTO }>('/users/barber', {
+      preHandler: authMiddleware,
+    },
+      async (request, reply) => {
+        return this.userController.createBarber(request, reply);
+      });
+
+    this.fastify.get<{ Params: { barbershopId: number } }>('/user/barbers/barbershop/:barbershopId', {
+      preHandler: authMiddleware,
+    }, async (request, reply) => {
+      return this.userController.findAllBarbersByBarbershopId(request, reply);
     });
 
     this.fastify.get('/users', async (request, reply) => {
@@ -22,12 +38,16 @@ export class UserRoutes {
       return this.userController.findById(request, reply);
     });
 
-    this.fastify.put<{ Params: { id: number }; Body: UpdateUserDTO }>('/users/:id', async (request, reply) => {
+    this.fastify.patch<{ Params: { id: number }; Body: UpdateUserDTO }>('/users/:id', async (request, reply) => {
       return this.userController.update(request, reply);
     });
 
     this.fastify.delete<{ Params: { id: number } }>('/users/:id', async (request, reply) => {
-      return this.userController.softDelete (request, reply);
+      return this.userController.softDelete(request, reply);
+    });
+
+    this.fastify.patch<{ Params: { id: number } }>('/users/:id/restore', async (request, reply) => {
+      return this.userController.restore(request, reply);
     });
   }
 }

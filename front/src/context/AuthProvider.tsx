88170f1cb97e321e9a571b5908551da02361200e
CaiqueOrtega <kayqueortega@gmail.com>
2024-11-29 02:53:27 -0300

    feat(auth): implement AuthContext and AuthProvider for user authentication
    - Created `AuthContext` to manage authentication state across the application.
    - Implemented `AuthProvider` component to handle login, logout, and authentication verification.
    - Integrated token decoding with `decodeToken` to store decoded user data in context.
    - Added `useAuth` hook for easy access to authentication state and functions in any component.
    - Handled loading state while checking for user authentication status on app initialization.
    - Supports login and logout functionality with updates to `user` and `isAuthenticated`.

---
+++ front/src/context/AuthProvider.tsx
@@ -0,0 +1,63 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { decodeToken } from "../utils/jwtUtils";
+import AuthServices from "../services/auth/authServices";
+import { AuthContextType, DecodedToken } from "../types/auth";
+
+
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [user, setUser] = useState<DecodedToken | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const { verifyAuth } = AuthServices();
+
+  useEffect(() => {
+    const verifyAuthentication = async () => {
+      try {
+        const response = await verifyAuth();
+        if (response.success && response.token) {
+          const decoded = decodeToken(response.token);
+          if (decoded) {
+            setUser(decoded);
+            setIsAuthenticated(true);
+          }
+        }
+      } catch (error) {
+        console.error("Erro ao verificar autenticação:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    verifyAuthentication();
+  }, []);
+
+  const login = (token: string) => {
+    const decoded = decodeToken(token);
+    if (decoded) {
+      setUser(decoded);
+      setIsAuthenticated(true);
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, user, loading, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth deve ser usado dentro de um AuthProvider");
+  }
+  return context;
+};

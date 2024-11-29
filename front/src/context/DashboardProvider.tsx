931839e2ed495c21cffedd24e775fc367d10099a
CaiqueOrtega <kayqueortega@gmail.com>
2024-11-29 02:53:48 -0300

    feat(dashboard): implement DashboardContext and DashboardProvider
    - Created `DashboardContext` to manage dashboard data (barbershops, barber).
    - Implemented `DashboardProvider` to provide global state for dashboard data throughout the app.
    - Added `useDashboard` hook to easily access and update dashboard data in components.
    - Ensured proper error handling if `useDashboard` is used outside of the `DashboardProvider`.
    - Initialized default state for dashboard data (empty arrays for barbershops and barber).

---
+++ front/src/context/DashboardProvider.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useContext, ReactNode } from 'react';
+import { DashboardContextType, DashboardData } from '../types/dashboard';
+
+const DashboardContext = createContext<DashboardContextType | undefined>(undefined);
+
+interface DashboardProviderProps {
+    children: ReactNode;
+}
+
+export const DashboardProvider: React.FC<DashboardProviderProps> = ({ children }) => {
+    const [dashboardData, setDashboardData] = useState<DashboardData>({
+        barbershops: [],
+        barber: [],
+    });
+
+    return (
+        <DashboardContext.Provider value={{ dashboardData, setDashboardData }}>
+            {children}
+        </DashboardContext.Provider>
+    );
+};
+
+export const useDashboard = () => {
+    const context = useContext(DashboardContext);
+    if (!context) {
+        throw new Error('useDashboard must be used within a DashboardProvider');
+    }
+    return context;
+};

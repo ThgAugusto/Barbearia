de15e4d1dfdea67d915b2c7782446bed5a2fde56
CaiqueOrtega <kayqueortega@gmail.com>
2024-11-29 02:50:30 -0300

    refactor(App): simplify router setup
    - Removed unnecessary `RouterProvider` and directly rendered the `Router` component in the root.

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
 import './assets/styles/index.css'
 import { Router } from './routes/router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <RouterProvider router={Router} />
+      <Router />
   </StrictMode>,
 )
